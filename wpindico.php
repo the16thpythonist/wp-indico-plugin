1191ca0d8deb7c8bd2f2488b59894a07461f6d3c
the16thpythonist <jonseb1998@gmail.com>
2019-01-04 08:07:00 +0100

    initial commit

---
+++ wpindico.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: WpIndico
+ * Plugin URI: https://github.com/the16thpythonist/wp-scopus-plugin
+ * Description: Wordpress Indico event management
+ * Author: Jonas Teufel
+ * Version: 0.0.0.0
+ * Author URI: google.com
+ * License: GPLv2 or later
+ */
+
+namespace WpIndico;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+
